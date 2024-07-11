110459aa3aa2cf073bd839b635ba1fb3d6d075b1
MValMich <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIId/a1uZVx9qurXnWldA7h3BQ4ON3OTVGnBmr2+ZhMsP marvalmich@gmail.com>
2024-07-11 12:11:03 -0400

    SUJETO 1: Se estructura el sitio web con los elementos HTML. OBJETO 1: Elementos necesarios para el buen desarrollo del sitio web.    SUJETO 2: Se crea el codigo y la logica con JavaScript. OBJETO 2: Con el fin de dar interactividad al sitio web con el usuario.    SUJETO 3: Se desarrolla el codigo CSS y se da estilo personalizado al sitio web. OBJETO 3: Con el fin de hacer mas entretenido y atractivoel sitio web para el usuario

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Proyecto-Libreria
+
+OBJETIVOS: 
+1) Crear una Libreria. Será un sitio-web en el que el usuario tendrá  que rellenar un Formulario ingresando información relevante sobre algun libro de su interes, la que posteriormente será impresa en pantalla. 
+2) Aplicar los conceptos aprendidos del curso de Java Script sobre Objetos (Constructor de Objetos, Prototipos, Herencias, etc )
+3) Manipular el DOM (a travez de la creación de nuevos elementos, manipular Eventos de escucha, y aplicar el estilo CSS de los nuevos elementos).
+4) Esatructurar el sitio-web con los elementos HTML necesarios (incorporando un formulario en forma de dialogo, con sus demas elementos necesarios)
+5) Dar estilo personalizado al sitio-web con CSS. 
---
+++ script.js
@@ -0,0 +1,67 @@
+// JS: Proyecto-Libreria
+const formulario = document.querySelector('#formulario');
+const dialog = document.querySelector('dialog');
+const idTitulo = document.querySelector('#id-titulo');
+const idAutor = document.querySelector('#id-autor');
+const idPaginas = document.querySelector('#id-paginas');
+const idLeido = document.querySelector('#id-leido');
+const formBtn = document.querySelector('#form-btn');
+const container = document.querySelector('#container');
+const miLibreria = [];
+
+function Libro(titulo, autor, paginas, leido) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.paginas = paginas;
+    this.leido = leido ? 'Leído' : 'No leído';
+    this.info = function() {
+        return 'Título: ' + this.titulo + ' / ' +' Autor: ' + this.autor + ' / ' +' Páginas: ' + this.paginas + ' / ' +' Estado: ' + this.leido;
+    };
+}
+
+function añadirLibroLibreia() {
+    const titulo = idTitulo.value.replace(/[&<>'"]/gi,"");
+    const autor = idAutor.value.replace(/[&<>'"]/gi,"");
+    const paginas = idPaginas.value.replace(/[&<>'"]/gi,"");
+    const leido = idLeido.checked;
+    const nuevoLibro = new Libro(titulo, autor, paginas, leido);
+    miLibreria.push(nuevoLibro);
+    mostrarLibroPatalla(nuevoLibro);
+}
+
+// FORMULARIO
+function desplegarFormulario() {
+    formBtn.addEventListener('click', () => {
+        dialog.showModal();
+    });
+}
+desplegarFormulario();
+'Subir'
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault();
+    añadirLibroLibreia();
+    dialog.close();
+});
+'Cerrar'
+const cerrarBtn = document.querySelector('#cerrar-btn')
+cerrarBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    dialog.close();
+});
+
+// CREAR NUEVO LIBRO
+function mostrarLibroPatalla(libro) {
+    const content = document.createElement('div');
+    content.classList.add('class-content');
+    content.textContent = libro.info();
+    container.appendChild(content);
+
+    const borrarLibroBtn = document.createElement('button')
+    borrarLibroBtn.classList.add('borrar-btn')
+    borrarLibroBtn.textContent = 'Borrar'
+    content.appendChild(borrarLibroBtn);
+    'borrar'
+ borrarLibroBtn.addEventListener('click', () => {
+        container.removeChild(content);
+    });
+}
