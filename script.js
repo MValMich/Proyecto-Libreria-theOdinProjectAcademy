8fe683d367f9569abde79ccef68a39ee3a15b5b7
MValMich <electrnico 155995288-MValMich.users.noreply.github.com>
2025-08-07 10:27:30 -0400

    Sujeto: Se trabaja en la corrección del campo checkin. Objeto: Ya que la app no estaba funcionando debido a error en ese campo

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function añadirLibroLibreia() {
     const titulo = idTitulo.value.replace(/[<>{}()'"&%\-=:#?|]/gi,"");
     const autor = idAutor.value.replace(/[<>{}()'"&%\-=:#?|]/gi,"");
     const paginas = idPaginas.value.replace(/[<>{}()'"&%\-=:#?|]/gi,"");
-    const leido = idLeido.checked.replace(/[<>{}()'"&%\-=:#?|]/gi,"");
+    const leido = idLeido.checked
     const nuevoLibro = new Libro(titulo, autor, paginas, leido);
     miLibreria.push(nuevoLibro);
     mostrarLibroPatalla(nuevoLibro);
